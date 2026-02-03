66ee86e97883973e2d873c1b6e2808e457f55fe9
crousty24-bit <allen.kch24@gmail.com>
2026-02-03 17:29:38 +0100

    feat: Add game logic, fight rounds between 2 players
    add 2 new players & display players state before fight begins
    
    add while loop to run attacks between player1 & player 2; end when player HP down to 0
    
    add condition break if player HP down to 0: enable to counter attack if dead

--- app.rb
+++ app.rb
@@ -4,4 +4,24 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
+player1 = Player.new("Gork")
+player2 = Player.new("Sudo")
+
+puts "Players ready in arena :"
+puts "#{player1.show_state()}"
+puts "#{player2.show_state()}"
+
+while player1.life_points > 0 && player2.life_points > 0
+  puts "Let the fight begin !"
+  player2.attacks(player1)
+  if player1.life_points <= 0
+    break
+  end
+  puts "#{player1.show_state()}"
+  player1.attacks(player2)
+  if player2.life_points <= 0
+    break
+  end
+  puts "#{player2.show_state()}"
+end
 binding.pry
