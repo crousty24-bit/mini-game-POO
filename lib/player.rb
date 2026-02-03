c28a561c755178735a34493a25a77c4af9c271d0
crousty24-bit <allen.kch24@gmail.com>
2026-02-03 15:57:28 +0100

    feat: Add object Player attributes & first game logic
    add name & starting life points when player is created
    
    add show_state method to display player name and HP remaining
    
    add logic player gets_damage, show if player's dead

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,20 @@
 require 'pry'
 class Player
   attr_accessor :name, :life_points
-  def initialize(name, life_points)
+  def initialize(name)
     @name = name
     @life_points = 10
+  end
+  def show_state()
+    puts "Player #{@name} has #{@life_points} HP left"
+  end
+  def gets_damage(damage_taken)
+    @life_points = @life_points - damage_taken
+    if @life_points > 0 
+      puts "Player #{@name} has taken #{damage_taken} damage !"
+    end
+    if @life_points <= 0
+      puts "Player #{@name} has been slayed by ennemy !"
+    end
   end
 end
