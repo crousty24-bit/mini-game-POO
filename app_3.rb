3b0070b7f49fa96d9e7bb0576c8333d93f2e7405
crousty24-bit <allen.kch24@gmail.com>
2026-02-04 11:38:49 +0100

    feat: add while loop with user input
    add Game.new to run game script

--- app_3.rb
+++ app_3.rb
@@ -5,3 +5,27 @@ require_relative 'lib/game'
 require_relative 'lib/player'
 
 # v3.0
+puts "------------------------------------------------"
+puts "|         Welcome to THP Battle Arena.         |"
+puts "|     Last Dev standing is the winner, yay!    |"
+puts "-------------------------------------------------"
+
+puts "Please enter your gamer tag : "
+user_name = gets.chomp
+my_game = Game.new(user_name)
+
+while my_game.is_still_ongoing?
+  my_game.show_players
+  my_game.new_players_in_sight
+  my_game.menu
+  
+  puts "ENTER :"
+  action = gets.chomp
+  my_game.menu_choice(action)
+  
+  my_game.enemies_attack
+  
+  puts "-------------------------------------------"
+end
+
+my_game.end_game
